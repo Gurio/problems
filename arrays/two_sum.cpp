6b135982e34551ac35b6509a73cd85eeb6286815
Arseni Lapunov <re.stage00101@gmail.com>
2018-04-20 21:21:01 +0200

    init

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,27 @@
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector <int> twoSum (vector<int>& nums, int target){
+    unordered_map<int,int> num_pos;
+    for (size_t i = 0; i<nums.size(); i++){
+        int desired_num = target - nums[i];
+        int addend_idx = num_pos[desired_num];
+        if (addend_idx != 0){
+            return {addend_idx-1, i};
+        }
+        num_pos[nums[i]] = i+1;
+    }
+    return {0,0};
+}
+
+int main(){
+    vector<int> input = {2,7,11,15};
+    vector<int> result = twoSum(input, 9);
+    for (std::vector<int>::const_iterator i = result.begin(); i != result.end(); ++i)
+        cout << *i << ' '; 
+    cout << endl;
+    return 0;
+}
---
+++ lists/add_two_numbers_rev_list.cpp
@@ -0,0 +1,93 @@
+/*
+ *
+DIGITES ARE REVERSED!
+======================
+there is no need to append zeroes, or compare length
+So please don't
+ *
+ */
+
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+int lenDiff (ListNode* l1, ListNode* l2){
+    int diff = 0;
+    while (l1 && l2){
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    while (l1){
+        ++diff;
+        l1 = l1->next;
+    }
+    while (l2){
+        --diff;
+        l2 = l2->next;
+    }
+    return diff;
+}
+
+ListNode* prependZeroes(ListNode* l, int zeroes){
+    ListNode *result = l;
+    while (l->next){
+        l = l->next;
+    }
+    for (int i=0; i<zeroes; i++){
+        ListNode *zero = new ListNode(0);
+        l->next = zero;
+        l = zero;
+    }
+    return result;
+}
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int len_diff = lenDiff(l1,l2);
+    if (len_diff < 0){
+        l1 = prependZeroes(l1,-len_diff);
+    } else {
+        l2 = prependZeroes(l2,len_diff);
+    }
+    int rem = 0;
+    ListNode* result = new ListNode(0);
+    ListNode* sum = result;
+    while (l1){
+        sum->val = l1->val + l2->val + rem;
+        rem = sum->val/10;
+        sum->val %= 10;
+        l1 = l1->next;
+        l2 = l2->next;
+        if (l1 || rem){
+            sum->next = new ListNode(rem);
+            sum = sum->next;
+        }
+    }
+    return result;
+}
+
+int main (){
+    ListNode *l1 = new ListNode (9);
+    ListNode *l2 = new ListNode (1);
+    l1->next = new ListNode (8);
+    //l1->next->next = new ListNode (3); 
+    //l2->next = new ListNode (6);
+    //l2->next->next = new ListNode (4);
+
+    ListNode* result = addTwoNumbers(l1,l2);
+    while (result){
+        cout << result->val << ' ';
+        result = result->next;
+    } 
+    cout << endl;
+
+    return 0;
+}
